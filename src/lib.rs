7e455b537ae1a2e21cd878d6f41aaad431e26df8
Colin Roberts <colin@primitive.xyz>
2025-04-27 10:09:23 -0600

    feat: simplex

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![doc = include_str!("../README.md")]
 #![warn(missing_docs)]
-#![feature(generic_const_exprs)]
 
 pub mod definitions;
 pub mod graph;
--- src/simplicial.rs
+++ src/simplicial.rs
@@ -1,20 +1,48 @@
-pub struct Simplex<V, const K: usize>
-where [(); K + 1]: {
+use std::array;
+
+use itertools::Itertools;
+
+pub struct Simplex<V> {
   // A simplex of dimension K has K+1 vertices
-  vertices: [V; K + 1],
+  vertices: Vec<V>,
+  dimension: usize,
 }
 
-impl<V: Clone + PartialEq, const K: usize> Simplex<V, K>
-where [(); K + 1]:
-{
+impl<V: Clone + PartialEq> Simplex<V> {
   // Create a new simplex from K+1 vertices
-  pub fn new(vertices: [V; K + 1]) -> Self { Self { vertices } }
+  pub fn new(dimension: usize, vertices: Vec<V>) -> Self {
+    assert!(vertices.iter().combinations(2).all(|v| v[0] != v[1]));
+    assert!(vertices.len() == dimension + 1);
+    Self { vertices, dimension }
+  }
 
   // Get a reference to the vertices
-  pub fn vertices(&self) -> &[V; K + 1] { &self.vertices }
+  pub fn vertices(&self) -> &[V] {
+    &self.vertices
+  }
 
-  pub fn boundary(&self) -> Vec<Simplex<V, { K - 1 }>>
-  where [(); (K - 1) + 1]: /* <- THIS is needed for Simplex<V, {K-1}> */ {
-    todo!()
+  pub fn faces(&self) -> Vec<Simplex<V>> {
+    self
+      .vertices
+      .clone()
+      .into_iter()
+      .combinations(self.dimension)
+      .map(|v| Self::new(self.dimension - 1, v))
+      .collect()
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_simplex_faces() {
+    let simplex = Simplex::new(2, vec![0, 1, 2]);
+    let faces = simplex.faces();
+    assert_eq!(faces.len(), 3);
+    assert_eq!(faces[0].vertices(), &[0, 1]);
+    assert_eq!(faces[1].vertices(), &[0, 2]);
+    assert_eq!(faces[2].vertices(), &[1, 2]);
   }
 }
